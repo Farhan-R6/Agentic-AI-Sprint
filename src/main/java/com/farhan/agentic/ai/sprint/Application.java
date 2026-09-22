d2cd2a17762d414b824e5b1f87c2d5cf6744c800
FarhanR675 <farhanrahman675@gmail.com>
2026-09-22 14:59:33 +0100

    Inital set up of project

---
+++ src/main/java/com/farhan/agentic/ai/sprint/Application.java
@@ -0,0 +1,13 @@
+package com.farhan.agentic.ai.sprint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/farhan/agentic/ai/sprint/controller/pdfController.java
@@ -0,0 +1,19 @@
+package com.farhan.agentic.ai.sprint.controller;
+
+import com.google.genai.types.File;
+import org.apache.pdfbox.pdmodel.interactive.annotation.handlers.PDFileAttachmentAppearanceHandler;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ask")
+public class pdfController {
+
+
+    public VectorStoreProvider pdfText(@RequestBody PDFileAttachmentAppearanceHandler pdf) {
+        return pdf.toString();
+    }
+
+}
