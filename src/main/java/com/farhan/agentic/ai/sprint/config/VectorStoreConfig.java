1c8590dabb5471420c155033138a1aa1d553e3c5
FarhanR675 <farhanrahman675@gmail.com>
2026-09-23 13:37:18 +0100

    VectorStore configured and handles embedding for rag

--- src/main/java/com/farhan/agentic/ai/sprint/config/VectorStoreConfig.java
+++ src/main/java/com/farhan/agentic/ai/sprint/config/VectorStoreConfig.java
@@ -1,4 +1,16 @@
 package com.farhan.agentic.ai.sprint.config;
 
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class VectorStoreConfig {
+
+    @Bean
+    VectorStore vectorStore(EmbeddingModel embeddingModel) {
+        return SimpleVectorStore.builder(embeddingModel).build();
+    }
 }
